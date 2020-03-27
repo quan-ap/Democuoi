5528cc5f62235f5a67e0e7cdc693238633472f9e
quansupermaket <62633212+quansupermaket@users.noreply.github.com>
2020-03-27 11:07:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
+
+html>
+<body>
+
+<h2>JavaScript in Body</h2>
+
+<p id="demo"></p>
 <?php echo "Hello World!" ?>
+<script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script>
+
+</body>
+</html>
