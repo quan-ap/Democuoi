52f071b528e2b850fd5863b9ab1aa89f61a99561
quansupermaket <62633212+quansupermaket@users.noreply.github.com>
2020-03-29 22:23:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
    $host = "ec2-52-87-58-157.compute-1.amazonaws.com";
    $dbname = "d6k7dcod066oe6";
    $user = "wfrhukopgnebzu";
-   $password = "0b112a6c9fa77ff1bbe9c23666b727cb56e8a47c4ea068ae0e6723b5f5129b60";
+   $password = "0b112a6c9fa77ff1bbe9c23666b727cb56e8a47c4ea068ae0e6723b5f5129b6";
    $port = "5432";
 
    $dsn = "pgsql:host=$host;dbname=$dbname;user=$user;port=$port;password=$password sslmode=require";
