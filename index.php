6037e078c1cfca88dfb82dc03584def2c9893f5e
quansupermaket <62633212+quansupermaket@users.noreply.github.com>
2020-03-29 22:10:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,7 @@ html>
 <?php echo "Hello World!" ?>
 <?php
 
-   $con = "dbname=d6k7dcod066oe6 host=ec2-52-87-58-157.compute-1.amazonaws.com port=5432 user=wfrhukopgnebzu password=
-0b112a6c9fa77ff1bbe9c23666b727cb56e8a47c4ea068ae0e6723b5f5129b60 sslmode=require";
+   $con = "dbname=Demo host=8080 port=3306 user=sa password=thaylinh sslmode=require";
 
 
    if (!$con) 
